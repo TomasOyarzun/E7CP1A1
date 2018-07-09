d8e377d87c697b88515e365dcf1fd3c3eb128b6b
Tomas <oyarzun.tomas@gmail.com>
2018-07-09 19:28:57 -0400

    Fixed code

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,6 +1,6 @@
 productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
 
-productos.each { |valor, producto| puts valor }
+productos.each { |key, value| puts value }
 productos['cereal'] = 2200
 productos['bebida'] = 2000
 a = productos.to_a
